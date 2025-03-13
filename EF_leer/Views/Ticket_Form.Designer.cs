b47a7f56a9bf463694398683013e80c50834c133
Jadon <jadonnoah06@gmail.com>
2025-03-13 12:20:58 +0100

    Ticket Erstellen Frontend

--- EF_leer/Views/Ticket_Form.Designer.cs
+++ EF_leer/Views/Ticket_Form.Designer.cs
@@ -32,7 +32,25 @@ namespace EF_leer
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBoxKunde = new System.Windows.Forms.ComboBox();
+            this.Kunde = new System.Windows.Forms.Label();
+            this.richTextBoxIntNotiz = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxIntStatus = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxArt = new System.Windows.Forms.ComboBox();
+            this.comboBoxPrio = new System.Windows.Forms.ComboBox();
+            this.comboBoxStatus = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTitel = new System.Windows.Forms.TextBox();
+            this.richTextBoxBesch = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -42,17 +60,34 @@ namespace EF_leer
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(200, 100);
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.richTextBoxBesch);
+            this.tabPage1.Controls.Add(this.textBoxTitel);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.comboBoxStatus);
+            this.tabPage1.Controls.Add(this.comboBoxPrio);
+            this.tabPage1.Controls.Add(this.comboBoxArt);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.richTextBoxIntStatus);
+            this.tabPage1.Controls.Add(this.richTextBoxIntNotiz);
+            this.tabPage1.Controls.Add(this.Kunde);
+            this.tabPage1.Controls.Add(this.comboBoxKunde);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Erstellen";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -60,20 +95,168 @@ namespace EF_leer
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(768, 400);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Übersicht";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxKunde
+            // 
+            this.comboBoxKunde.FormattingEnabled = true;
+            this.comboBoxKunde.Location = new System.Drawing.Point(49, 6);
+            this.comboBoxKunde.Name = "comboBoxKunde";
+            this.comboBoxKunde.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxKunde.TabIndex = 0;
+            // 
+            // Kunde
+            // 
+            this.Kunde.AutoSize = true;
+            this.Kunde.Location = new System.Drawing.Point(5, 9);
+            this.Kunde.Name = "Kunde";
+            this.Kunde.Size = new System.Drawing.Size(38, 13);
+            this.Kunde.TabIndex = 1;
+            this.Kunde.Text = "Kunde";
+            // 
+            // richTextBoxIntNotiz
+            // 
+            this.richTextBoxIntNotiz.Location = new System.Drawing.Point(5, 244);
+            this.richTextBoxIntNotiz.Name = "richTextBoxIntNotiz";
+            this.richTextBoxIntNotiz.Size = new System.Drawing.Size(233, 96);
+            this.richTextBoxIntNotiz.TabIndex = 2;
+            this.richTextBoxIntNotiz.Text = "";
+            // 
+            // richTextBoxIntStatus
+            // 
+            this.richTextBoxIntStatus.Location = new System.Drawing.Point(244, 244);
+            this.richTextBoxIntStatus.Name = "richTextBoxIntStatus";
+            this.richTextBoxIntStatus.Size = new System.Drawing.Size(235, 96);
+            this.richTextBoxIntStatus.TabIndex = 3;
+            this.richTextBoxIntStatus.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Int. Notiz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Int. Status";
+            // 
+            // comboBoxArt
+            // 
+            this.comboBoxArt.FormattingEnabled = true;
+            this.comboBoxArt.Location = new System.Drawing.Point(8, 95);
+            this.comboBoxArt.Name = "comboBoxArt";
+            this.comboBoxArt.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxArt.TabIndex = 6;
+            // 
+            // comboBoxPrio
+            // 
+            this.comboBoxPrio.FormattingEnabled = true;
+            this.comboBoxPrio.Location = new System.Drawing.Point(137, 95);
+            this.comboBoxPrio.Name = "comboBoxPrio";
+            this.comboBoxPrio.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPrio.TabIndex = 7;
+            // 
+            // comboBoxStatus
+            // 
+            this.comboBoxStatus.FormattingEnabled = true;
+            this.comboBoxStatus.Location = new System.Drawing.Point(264, 95);
+            this.comboBoxStatus.Name = "comboBoxStatus";
+            this.comboBoxStatus.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxStatus.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Art";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Priorität";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(261, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Status";
+            // 
+            // textBoxTitel
+            // 
+            this.textBoxTitel.Location = new System.Drawing.Point(49, 47);
+            this.textBoxTitel.Name = "textBoxTitel";
+            this.textBoxTitel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTitel.TabIndex = 12;
+            // 
+            // richTextBoxBesch
+            // 
+            this.richTextBoxBesch.Location = new System.Drawing.Point(8, 149);
+            this.richTextBoxBesch.Name = "richTextBoxBesch";
+            this.richTextBoxBesch.Size = new System.Drawing.Size(287, 66);
+            this.richTextBoxBesch.TabIndex = 13;
+            this.richTextBoxBesch.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Titel";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Beschreibung";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 27);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Erstellen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Ticket_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 453);
             this.Controls.Add(this.tabControl1);
             this.Name = "Ticket_Form";
             this.Text = "Ticket_Form";
+            this.Load += new System.EventHandler(this.Ticket_Form_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -83,5 +266,22 @@ namespace EF_leer
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBoxBesch;
+        private System.Windows.Forms.TextBox textBoxTitel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxStatus;
+        private System.Windows.Forms.ComboBox comboBoxPrio;
+        private System.Windows.Forms.ComboBox comboBoxArt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxIntStatus;
+        private System.Windows.Forms.RichTextBox richTextBoxIntNotiz;
+        private System.Windows.Forms.Label Kunde;
+        private System.Windows.Forms.ComboBox comboBoxKunde;
+        private System.Windows.Forms.Button button1;
     }
 }
--- EF_leer/Views/Ticket_Form.cs
+++ EF_leer/Views/Ticket_Form.cs
@@ -15,7 +15,25 @@ namespace EF_leer
         public Ticket_Form()
         {
             InitializeComponent();
-            int i = 2;
+            
+        }
+
+        private void Ticket_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Kunde = comboBoxKunde.SelectedIndex;
+            string Titel = textBoxTitel.Text;
+            int Art = comboBoxArt.SelectedIndex;
+            int Prio = comboBoxPrio.SelectedIndex;
+            int Status = comboBoxStatus.SelectedIndex;
+            string Beschreibung = richTextBoxBesch.Text;
+            string IntNotiz = richTextBoxIntNotiz.Text;
+            string IntStatus = richTextBoxIntStatus.Text;
+            DateTime Erstelldatum = new DateTime();
         }
     }
 }
